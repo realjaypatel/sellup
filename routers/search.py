ff913c64332ef964c197f24682a59316f6c0a0e6
Jaykumar Patel <jaykumarpatel@acies.holdings>
2024-12-02 08:28:57 +0530

    all done

--- routers/search.py
+++ routers/search.py
@@ -8,22 +8,39 @@ templates = Jinja2Templates(directory="templates")
 
 
 router = APIRouter(
-    prefix='/search',
-    tags=['search']
+    prefix='/explore',
+    tags=['explore']
 )
-
-data = {
-    
-}
 
 
 
 
 @router.get('/', status_code=status.HTTP_200_OK)
-async def return_home(request: Request):
+async def return_home(request: Request,search):
     data = rq.get('https://excel2api.vercel.app/api/1BSOoMb-j3ALwi56lgSW8x7q17iNGSbuq1gpi9vV_ZOQ')
     data = data.json()
+    if not search:        
+        return templates.TemplateResponse("search.html", {"request": request,"data":data})
+    else:
+        search_result = []
+        for value in data:
+            if search.lower() in value['Title'].lower():
+                search_result.append(value)
+        return templates.TemplateResponse("search.html", {"request": request,"data":search_result})
+
     
-    return templates.TemplateResponse("search.html", {"request": request,"data":data})
-    
+
+@router.get('/categories/{category}', status_code=status.HTTP_200_OK)
+async def return_home(request: Request,category):
+    data = rq.get('https://excel2api.vercel.app/api/1BSOoMb-j3ALwi56lgSW8x7q17iNGSbuq1gpi9vV_ZOQ')
+    data = data.json()
+    if not category:
+        return templates.TemplateResponse("search.html", {"request": request,"data":data})
+    else:
+        search_result = []
+        for value in data:
+            if category.lower() in value['Category'].lower():
+                search_result.append(value)
+        return templates.TemplateResponse("search.html", {"request": request,"data":search_result})
+
 
